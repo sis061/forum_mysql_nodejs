d514cb5899a312982407300b8dba524328bff528
sis061 <sis061@naver.com>
2023-11-08 11:23:35 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BoardList from "./BoardList";
+import Write from "./Write";
+import Read from "./Read";
+import Modify from "./Modify";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BoardList />} />
+          <Route path="/write" element={<Write />} />
+          <Route path="/read/:id" element={<Read />} />
+          <Route path="/modify/:id" element={<Modify />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
-
+};
 export default App;
